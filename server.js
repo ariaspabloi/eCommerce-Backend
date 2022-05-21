dac31be22438c015d70c267e9ee2c11c1f5477fe
Pablo Arias <pabloarias.ic@gmail.com>
2022-05-21 18:32:20 -0400

    Plantilla Pug 2

--- server.js
+++ server.js
@@ -2,10 +2,25 @@
 const express = require('express')
 const bp = require('body-parser')
 const { apiControllers } = require('./controllers/apiControllers.js')
-const { routerApiProducts } = require('./routers/routerApiProducts.js')
+const { routerApiProducts } = require('./routers/routerApiProducts.js');
+const { ProductDao } = require('./databases/ProductDao.js');
+const productDao = new ProductDao("productos.txt");
 const app = express()
+app.set('view engine', 'pug');
+app.use(express.urlencoded({
+    extended: true
+  }))
+app.get('/', (req, res) => {
+    res.render('index', {title:"Agrega un producto"});
+});
+app.get('/productos', async (req, res) =>  {
+    res.render('productos', {title:"Productos",products: await productDao.getAll()});
+});
+app.post('/productos', (req, res) => {
+    productDao.save(req.body);
+    res.render('index', {title:"Agregado",loaded: true});
+});
 
-app.get('/', apiControllers.info)
 app.use(routerApiProducts)
 
 const PORT = 8080
