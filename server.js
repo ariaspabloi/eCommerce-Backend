b6c726840673ca2b9a795a27a53e2838d31943f3
Pablo Arias <pabloarias.ic@gmail.com>
2022-05-21 19:07:21 -0400

    Plantilla HandleBars2

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 //npm run start:dev
 const express = require('express')
 const bp = require('body-parser')
+const exphbs = require('express-handlebars')
 const { apiControllers } = require('./controllers/apiControllers.js')
 const { routerApiProducts } = require('./routers/routerApiProducts.js')
+const { ProductDao } = require('./databases/ProductDao.js')
+const productDao = new ProductDao("productos.txt");
 const app = express()
 
-app.get('/', apiControllers.info)
+const handlebarsConfig = {
+    defaultLayout: 'index.handlebars'
+}
+app.use(express.urlencoded({
+    extended: true
+}))
+app.engine('.handlebars', exphbs.engine({ extname: '.handlebars', defaultLayout: "index"}));
+app.set('views', './views')
+app.set('view engine', 'handlebars')
+
+
+
+app.get('/', (req, res) => {
+    res.render('index', {title:"Agrega un producto"});
+});
+app.get('/productos', async (req, res) =>  {
+    res.render('productos', {title:"Productos",products: await productDao.getAll()});
+});
+app.post('/productos', (req, res) => {
+    productDao.save(req.body);
+    res.render('index', {title:"Agregado",loaded: true});
+});
+
 app.use(routerApiProducts)
 
 const PORT = 8080
