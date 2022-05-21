44e0c834eaf61920ab5e939a190e4fe5154758bf
Pablo Arias <pabloarias.ic@gmail.com>
2022-05-21 19:55:26 -0400

    Plantilla EJS

--- server.js
+++ server.js
@@ -1,27 +1,20 @@
 //npm run start:dev
 const express = require('express')
 const bp = require('body-parser')
-const exphbs = require('express-handlebars')
 const { apiControllers } = require('./controllers/apiControllers.js')
 const { routerApiProducts } = require('./routers/routerApiProducts.js')
 const { ProductDao } = require('./databases/ProductDao.js')
 const productDao = new ProductDao("productos.txt");
 const app = express()
 
-const handlebarsConfig = {
-    defaultLayout: 'index.handlebars'
-}
 app.use(express.urlencoded({
     extended: true
 }))
-app.engine('.handlebars', exphbs.engine({ extname: '.handlebars', defaultLayout: "index"}));
-app.set('views', './views')
-app.set('view engine', 'handlebars')
-
+app.set('view engine', 'ejs');
 
 
 app.get('/', (req, res) => {
-    res.render('index', {title:"Agrega un producto"});
+    res.render('index', {title:"Agrega un producto",loaded: false});
 });
 app.get('/productos', async (req, res) =>  {
     res.render('productos', {title:"Productos",products: await productDao.getAll()});
